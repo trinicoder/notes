a6c120f175cbf4466aa57588cff6ab166e75ee0e
Varoon S <varoonx@gmail.com>
2019-02-27 14:07:26 -0400

    added new css styles

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -8,9 +8,14 @@ export default () => {
     return (
       <div>
         <PrivateHeader title={"Dashboard"}/>
-        <div className="page-content">   
-          <Editor/>                   
+        <div className="page-content">             
+          <div className="page-content__sidebar">
           <NoteList/>
+          </div>               
+          <div className="page-content__main">
+          <Editor/>  
+          </div>  
+          
         
         </div>        
       </div>
